4d4d1e53bb892903064c04714fb18b232a516f43
Zaid Nawaz <zaidalviza786@gmail.com>
2026-01-16 05:00:24 +0500

    Reverted the pushed code

--- backend/app/api/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1 import auth, websocket
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(websocket.router, tags=["signaling"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(interviews.router, prefix="/interviews", tags=["interviews"])
